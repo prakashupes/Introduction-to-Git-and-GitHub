57c8fcc2fb1484275a9ab9822ed7ca55fefdb178
prakashupes <85prakash2017@gmail.com>
2020-04-06 17:50:29 +0530

    src

---
+++ 001_Basics/directory_diff_and_patch/dir1/file1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=6;
+	if(a>5)
+	cout<<a;
+
+}
---
+++ 001_Basics/directory_diff_and_patch/dir2/file2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=4;
+	if(a<5)
+	{
+		cout<<a;
+	}
+		
+	return 0;
+}
